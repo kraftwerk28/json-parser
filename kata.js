491d036195930fc9eb5711a354797f3c090e259a
kraftwerk28 <kefirchik3@gmail.com>
2018-03-14 16:28:35 +0200

    fin release

--- kata.js
+++ kata.js
@@ -12,34 +12,63 @@ const parse = str => {
   };
 
   const split = content => {
+    if (content.match(/^,|,$/))
+      throw new Error('invalid content');
     const arr = [];
 
     let curly = 0; // {}
     let square = 0 // []
+    let quotas = false // ""
     let lastI = 0;
 
     for (let i = 0; i <= content.length; i++) {
-      switch (content[i]) {
-        case '{':
-          curly++
-          break;
-        case '}':
-          curly--;
-          break;
-        case '[':
-          square++;
-          break;
-        case ']':
-          square--;
-          break;
+      if (!quotas) {
+        switch (content[i]) {
+          case '{':
+            curly++;
+            break;
+          case '}':
+            curly--;
+            break;
+          case '[':
+            square++;
+            break;
+          case ']':
+            square--;
+            break;
+        }
       }
+      if (content[i] === '"')
+        quotas = !quotas;
 
-      if (curly === 0 && square === 0 && (content[i] === ',' || i >= content.length)) {
-        let cont = content.slice(lastI, i++);
+      // switch (content[i]) {
+      //   case '{':
+      //     if (!quotas)
+      //       curly++
+      //       break;
+      //   case '}':
+      //     if (!quotas)
+      //       curly--;
+      //     break;
+      //   case '[':
+      //     if (!quotas)
+      //       square++;
+      //     break;
+      //   case ']':
+      //     if (!quotas)
+      //       square--;
+      //     break;
+      //   case '"':
+      //     quotas = !quotas;
+      //     break;
+      // }
+
+      if (curly === 0 && square === 0 && !quotas && (content[i] === ',' || i >= content.length)) {
+        let cont = content.slice(lastI, i);
         cont = clearSpaces(cont);
         if (cont.length > 0)
           arr.push(cont);
-        lastI = i;
+        lastI = i + 1;
       }
     }
 
@@ -50,40 +79,41 @@ const parse = str => {
 
     val = clearSpaces(val);
 
+    if (val.match(/](.*),$|,[ \t]+,|^\[(.*),([ \t]|),(.*)\]$/))
+      throw new Error(`Invalid expression: "${val}"`);
+
     if (!isNaN(parseFloat(val))) { // number parse
-      if (val.match(/(^(0[0-9]|\.|,|\+)|[a-z]|\.\.|,(,| )|\.(.*)\.| )/i)) {
+      //if (val.match(/(^(0[0-9]|\.|,|\+)|[a-z]|\.\.|,(,| )|\.(.*)\.| )/i)) {
+      if (val.match(/,|\.$|^(0[0-9]|\+|,|\.)|[^(0-9|\.|,|\-)]|,(.*),|\.(.*)\./i)) {
         throw new Error(`Invalid number: "${val}"`);
         console.log(val);
       }
       return parseFloat(val);
-    }
-    else if (val[0] === '{' && val[val.length - 1] === '}') { // object parse
+    } else if (val[0] === '{' && val[val.length - 1] === '}') { // object parse
       let obj = {};
       const content = clearSpaces(val.slice(1, val.length - 1));
       const fields = split(content);
 
       if (fields.length < 1) return {};
       fields.forEach(val => {
-        const div = val.indexOf(':');
-        if (div > -1) {
+        const start = val.indexOf('"');
+        const end = val.slice(start + 1, val.length).indexOf('"') + start + 1;
+        const div = val.slice(end + 1, val.length).indexOf(':') + start + end + 1;
 
+        if (start > -1 && end > -1 && div > -1) {
           let key = clearSpaces(val.slice(0, div));
-          const start = key.indexOf('"');
-          const end = key.lastIndexOf('"');
-          if (start !== end && start > -1 && end > -1)
-            key = clearSpaces(key.slice(start + 1, end));
-          else throw new Error(`Invalid key: "${key}"`);
-
+          key = key.slice(1, key.length - 1);
           const value = val.slice(div + 1, val.length);
 
           obj[key] = parseVal(value);
+        } else {
+          throw new Error('Object used like array');
         }
-        // Object.defineProperty(obj, pair[0], { value: parseVal(pair[1]) });
+
       });
 
       return obj;
-    }
-    else if (val[0] === '[' && val[val.length - 1] === ']') { // array parse
+    } else if (val[0] === '[' && val[val.length - 1] === ']') { // array parse
       const array = [];
       const content = val.slice(1, val.length - 1);
 
@@ -95,19 +125,28 @@ const parse = str => {
       });
 
       return array;
-    }
-    else if ((val[0] === '"' || val[0] === '\'') && (val[val.length - 1] === '"' || val[val.length - 1] === '\'')) {
+
+    } else if (val[0] === '"' && val[val.length - 1] === '"') { // strnig parse
       const res = val.slice(1, val.length - 1);
-      if (res.match(/('|")/) && !res.match(/(\\'|\\")/))
+      if (res.match(/"/) && !res.match(/(\\'|\\")/))
         throw new Error(`Invalid string: "${res}"`);
       return res;
-    }
-    // else if (val === '') return val;
-    else if (val === 'true') return true;
+    } else if (val === 'true') return true;
     else if (val === 'false') return false;
     else if (val === 'null') return null;
     else throw new Error(`Invalid expression: "${val}"`);
+
+  };
+
+  const full = () => {
+    return str;
   };
 
   return parseVal(str);
 };
+
+// console.log(parse(require('fs').readFileSync('./tes.json', 'utf8')));
+// console.log(JSON.parse(require('fs').readFileSync('./tes.json', 'utf8')));
+// console.log(parse('2,23343'));
+// console.log(JSON.parse('2,232'));
+// console.log(parse('[{},{},[1,2,3]]'));
